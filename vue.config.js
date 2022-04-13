9f7bd0e36733dd91130a95740268fb14a8c5eb67
An <67582144+anastasia-vas@users.noreply.github.com>
2022-04-13 19:35:16 +0300

    changed vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/http-pets/" : "/",
 });
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/http-pets/" : "/",
+};
