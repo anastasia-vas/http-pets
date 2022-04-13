cf30cc60c838d83fcf158b010b84724afcf225e8
An <67582144+anastasia-vas@users.noreply.github.com>
2022-04-13 15:18:19 +0300

    added @media styles to StartPage

--- src/views/StartPage.vue
+++ src/views/StartPage.vue
@@ -23,6 +23,7 @@
   height: 100%;
   display: flex;
   justify-content: center;
+  padding: 0 15px;
   .main-content {
     text-align: center;
     display: flex;
@@ -33,27 +34,72 @@
       font-size: 30px;
       line-height: 1.2;
       margin: 0 0 40px 0;
+      @media (max-width: 1200px) {
+        font-size: 27px;
+        margin: 0 0 30px 0;
+      }
+      @media (max-width: 410px) {
+        font-size: 24px;
+      }
     }
     p {
       font-size: 25px;
       line-height: 1.2;
+      @media (max-width: 410px) {
+        font-size: 22px;
+      }
     }
     .pets-buttons {
       display: flex;
       justify-content: space-around;
       margin: 70px 0 0 0;
+      @media (max-width: 992px) {
+        width: 45%;
+        flex-direction: column;
+        justify-content: center;
+        align-self: center;
+        align-items: center;
+        margin: 40px 0 0 0;
+      }
+      @media (max-width: 600px) {
+        width: 70%;
+        margin: 40px 0 0 0;
+      }
+      @media (max-width: 410px) {
+        width: 90%;
+      }
       .button {
+        width: 25%;
+        @media (max-width: 992px) {
+          width: 80%;
+        }
         a {
           position: relative;
           display: flex;
+          justify-content: center;
           border: 4px solid #8f8f8f;
+          width: 100%;
           padding: 15px 80px;
           color: #fff;
           font-size: 27px;
           line-height: 1.2;
+          @media (max-width: 992px) {
+            padding: 15px 70px;
+            width: 100%;
+          }
+          @media (max-width: 786px) {
+            padding: 15px 55px;
+          }
+          @media (max-width: 410px) {
+            font-size: 24px;
+            border: 3px solid #8f8f8f;
+          }
         }
       }
       .dog {
+        @media (max-width: 992px) {
+          margin: 0 0 70px 0;
+        }
         a {
           &::before {
             content: url("../assets/left_dogs_ear.svg");
@@ -76,12 +122,18 @@
             position: absolute;
             top: -98%;
             left: 1%;
+            @media (max-width: 410px) {
+              top: -101%;
+            }
           }
           &::after {
             content: url("../assets/right_cats_ear.svg");
             position: absolute;
             top: -98%;
             right: 1%;
+            @media (max-width: 410px) {
+              top: -101%;
+            }
           }
         }
       }
